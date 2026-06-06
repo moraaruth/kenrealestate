44e002735110672fafbd5e7bb86c16a2f63d3f97
Ruth Moraa Nyangau <RMNYANGAU@safaricom.co.ke>
2026-06-06 06:06:21 +0300

    changes to correct errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}
-
+      <body>
+        {children}
         <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Festatehub6703back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.18" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" />
+      </body>
     </html>
   );
 }
